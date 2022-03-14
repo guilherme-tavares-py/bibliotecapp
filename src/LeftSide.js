290964c4f630880ec08acd9cc1a990519f0302d1
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2022-03-14 13:42:50 -0300

    Small changes

---
+++ src/LeftSide.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class LeftSide extends React.Component {
+    render() {
+        return(
+            <div class="inner-left-container">
+                <h1>Bibliotecapp</h1>
+                <hr />
+                
+            </div>
+        )
+    }
+}
+
+
+export default LeftSide
